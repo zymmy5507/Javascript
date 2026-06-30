d3be2d0985098bac28bc2760d847071620df437b
Varun <darkvarun5507@gmail.com>
2026-06-30 23:21:02 +0530

    revise different methods

---
+++ Arrays/first.js
@@ -0,0 +1,18 @@
+
+// const myArr =  [1,2,"Varun",5,6,7,8,9,10];
+
+
+// console.log(myArr[2]);
+
+
+// these are different ways to create an array in javascript
+// const myArr2 = new Array(1,2,"varun",6,"Sharma");
+// myArr2.push("LastOne");
+// myArr2.unshift("0");
+// console.log(myArr2);
+
+// lets learn slice and spice methods in javascript
+const myArr3 = [1,2,3,4,5,6,7,8,9,10];
+console.log(myArr3.slice(2,5));
+console.log(myArr3.splice(2,5));
+console.log(myArr3);
---
+++ DataTypes/second.js
@@ -0,0 +1,7 @@
+let score = "33";
+
+
+let newScore = Number(score);
+
+console.log(typeof(score));
+console.log(typeof(newScore));

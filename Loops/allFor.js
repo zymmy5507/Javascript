2559a420f97967bbf0f3c33f2c01139f1965b66e
Varun <darkvarun5507@gmail.com>
2026-07-02 16:05:03 +0530

    delete unnessary things

--- Loops/allFor.js
+++ Loops/allFor.js
@@ -109,8 +109,4 @@ for( const key in obj) {
 
 // ++++++++++++++ FOR EACH +++++++++++++++++++++
 
-const myArray = [1, 2, 3, 4, 5];
 
-myArray.forEach((value, index) => {
-    console.log(`Index: ${index}, Value: ${value}`);
-}
